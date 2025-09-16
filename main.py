5526e6b2cc6fd786ff5adde54510379496200fdd
zcy831 <zcy831@hotmail.com>
2025-09-17 03:40:31 +0800

    add main file

---
+++ main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from typing import Union
+import uvicorn
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+if __name__ == "__main__":
+    uvicorn.run('main:app', host='0.0.0.0', port=8000, reload=True, workers=4)
